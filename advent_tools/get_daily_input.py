ae43ceff4d035b4bfc2057cbb7db69cdd5df1fe3
Jerry Gregoire <12764780+arcadecoffee@users.noreply.github.com>
2022-12-02 16:38:49 -0600

    tuned up for day 1 (#1)

--- advent_tools/get_daily_input.py
+++ advent_tools/get_daily_input.py
@@ -8,28 +8,37 @@ from typing import Iterable
 from urllib.request import Request, urlopen
 
 
+_URL_TEMPLATE = "https://adventofcode.com/2022/day/{day}/input"
+_SESSSION_FILENAME = ".aocsession"
+_CACHE_DIRECTORY = ".aoccache"
+
+
+def _cache_filename(day: int):
+    return f"{_CACHE_DIRECTORY}/{day}.txt"
+
+
 def download_daily_input(day: int = 1, session_id: str = None) -> None:
     """
     Yield lines from the day's input set
     """
     if not session_id:
-        with open(".aocsession", mode="rt") as dot_aocsession:
+        with open(_SESSSION_FILENAME, mode="rt") as dot_aocsession:
             session_id = dot_aocsession.readline().strip()
 
     with urlopen(Request(
-            url=f"https://adventofcode.com/2022/day/{day}/input",
+            url=_URL_TEMPLATE.format(day=day),
             headers={"Cookie": f"session={session_id}"}
         )) as response:
         if response.status == 200:
-            makedirs(".aoccache", exist_ok=True)
-            with open(f".aoccache/{day}.txt", mode="wt") as outfile:
+            makedirs(_CACHE_DIRECTORY, exist_ok=True)
+            with open(_cache_filename(day), mode="wt") as outfile:
                 for line in response:
                     outfile.write(line.decode())
 
 
 def get_daily_input(day: int = 1, session_id: str = None, force_download=False) -> Iterable[str]:
-    if force_download or not exists(f".aoccache/{day}.txt"):
+    if force_download or not exists(_cache_filename(day)):
         download_daily_input(day, session_id)
-    with open(f".aoccache/{day}.txt", mode="rt") as infile:
+    with open(_cache_filename(day), mode="rt") as infile:
         for line in infile:
             yield line.strip()
