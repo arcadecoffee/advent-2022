a15c94ee7521a0f6394cd96ba39da45a1fd1377d
Jerry Gregoire <jerry@arcadecoffee.com>
2022-12-03 15:26:52 -0600

    day 2 - part 2 - cleaned up

--- day02.py
+++ day02.py
@@ -15,48 +15,44 @@ def calculate_score(a: str, b: str) -> int:
         "paper": {"rock": 0, "paper": 3, "scissors": 6},
         "scissors": {"rock": 6, "paper": 0, "scissors": 3}
     }
-
     move_played_values = {"rock": 1, "paper": 2, "scissors": 3}
 
     return win_loss_values[a][b] + move_played_values[b]
+
+
+def analyze_strategy(version_2: bool = False):
+    move_translation = ["rock", "paper", "scissors"]
+
+    total_score = 0
+    for line in get_daily_input(DAY):
+        a, b = line.split(" ")
+        a_move = ord(a) - ord("A")
+
+        if version_2:
+            b_move = a_move + (ord(b) - ord("X") - 1)
+            if b_move > 2:
+                b_move = 0
+        else:
+            b_move = ord(b) - ord("X")
+
+        total_score += calculate_score(move_translation[a_move],
+                                       move_translation[b_move])
+
+    return total_score
 
 
 def part_1() -> int:
     """
     Solve part 1
     """
-    translation = ["rock", "paper", "scissors"]
-
-    total_score = 0
-    for line in get_daily_input(DAY):
-        a, b = line.split(" ")
-        total_score += calculate_score(
-            translation[ord(a) - ord("A")], translation[ord(b) - ord("X")]
-        )
-
-    return total_score
+    return analyze_strategy()
 
 
 def part_2() -> int:
     """
     Solve part 2
     """
-    move_translation = ["rock", "paper", "scissors"]
-    outcome_translation = [-1, 0, 1]
-
-    total_score = 0
-    for line in get_daily_input(DAY):
-        a, b = line.split(" ")
-        a_move = ord(a) - ord("A")
-        b_move = a_move + outcome_translation[ord(b) - ord("X")]
-        if b_move > 2:
-            b_move = 0
-
-        total_score += calculate_score(
-            move_translation[a_move], move_translation[b_move]
-        )
-
-    return total_score
+    return analyze_strategy(version_2=True)
 
 
 def main():
