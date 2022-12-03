920f9181c9341d765f123557d861ab1df5470169
Jerry Gregoire <jerry@arcadecoffee.com>
2022-12-03 16:15:59 -0600

    Day 4 - prep

---
+++ day04.py
@@ -0,0 +1,38 @@
+"""
+Advent of Code 2022 Day 4
+"""
+from advent_tools import get_daily_input
+
+DAY = 4
+
+DEBUG = False
+if DEBUG:
+    def get_daily_input(x):
+        rows = []
+        for row in rows:
+            yield row
+
+
+def part_1() -> int:
+    """
+    Solve part 1
+    """
+    total = 0
+    return total
+
+
+def part_2() -> int:
+    """
+    Solve part 2
+    """
+    total = 0
+    return total
+
+
+def main():
+    print(f"Part 1: {part_1()}")
+    print(f"Part 2: {part_2()}")
+
+
+if __name__ == "__main__":
+    main()
