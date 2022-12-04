d635204b3793d8b727e77b1e44cccc0a71212041
Jerry Gregoire <jerry@arcadecoffee.com>
2022-12-04 14:46:17 -0600

    Day 4 - Part 1

--- day04.py
+++ day04.py
@@ -1,16 +1,28 @@
 """
 Advent of Code 2022 Day 4
 """
+import re
+import sys
+
 from advent_tools import get_daily_input
 
 DAY = 4
 
-DEBUG = False
+DEBUG = sys.argv[1] == "debug" if len(sys.argv) > 1 else False
+
+DEBUG_DATA = """
+2-4,6-8
+2-3,4-5
+5-7,7-9
+2-8,3-7
+6-6,4-6
+2-6,4-8
+"""
+
 if DEBUG:
     def get_daily_input(x):
-        rows = []
-        for row in rows:
-            yield row
+        for line in DEBUG_DATA.strip().split("\n"):
+            yield line.strip()
 
 
 def part_1() -> int:
@@ -18,6 +30,11 @@ def part_1() -> int:
     Solve part 1
     """
     total = 0
+    for line in get_daily_input(DAY):
+        sections = [int(i) for i in  re.split(r"[,-]", line)]
+        if (sections[0] <= sections[2] and sections[1] >= sections[3]) or \
+                (sections[0] >= sections[2] and sections[1] <= sections[3]):
+            total += 1
     return total
 
 
