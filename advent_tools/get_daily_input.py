ffc14f793133dc50116f5c968daf8cf8a10688e9
Jerry Gregoire <12764780+arcadecoffee@users.noreply.github.com>
2022-12-07 21:29:25 -0600

    tweaking fetcher

--- advent_tools/get_daily_input.py
+++ advent_tools/get_daily_input.py
@@ -23,11 +23,12 @@ def download_daily_input(day: int = 1, session_id: str = None) -> None:
     """
     if not session_id:
         with open(_SESSION_FILENAME, mode="rt") as dot_aocsession:
+            useragent = dot_aocsession.readline().strip()
             session_id = dot_aocsession.readline().strip()
 
     with urlopen(Request(
             url=_URL_TEMPLATE.format(day=day),
-            headers={"Cookie": f"session={session_id}"}
+            headers={"Cookie": f"session={session_id}", "User-Agent": useragent}
         )) as response:
         if response.status == 200:
             makedirs(_CACHE_DIRECTORY, exist_ok=True)
