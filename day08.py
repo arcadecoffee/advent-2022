1e0717e11d82d13828a85623ba4a75c6f2fe19dd
Jerry Gregoire <12764780+arcadecoffee@users.noreply.github.com>
2022-12-08 16:08:45 -0600

    Merge pull request #9
    * Day 8 - tuning
    
    * Day 8 - tuning

--- day08.py
+++ day08.py
@@ -3,6 +3,8 @@ Advent of Code 2022 Day 8
 """
 import sys
 
+from typing import Iterable, NamedTuple
+
 from advent_tools import get_daily_input
 
 DAY = 8
@@ -18,85 +20,76 @@ DEBUG_DATA = """
 """
 
 if DEBUG:
-    def get_daily_input(x):
+    def get_daily_input(x) -> Iterable[str]:
         for line in DEBUG_DATA.strip().split("\n"):
             yield line.strip("\n")
 
 
-def load_forest() -> list[list[int]]:
-    forest = []
-    for input_row in get_daily_input(DAY):
-        tree_row = [int(height) for height in input_row]
-        forest.append(tree_row)
-    return forest
+Position = NamedTuple("Position", [("row", int), ("column", int)])
 
 
-def north(forest: list[list[int]], row: int, col: int) -> list[int]:
-    return [c[col] for c in reversed(forest[:row])]
+class Forest:
+    def __init__(self, data: Iterable[str] = None):
+        self.trees = []
+        for data_row in data:
+            self.trees.append([int(tree) for tree in data_row])
 
+        self.visibility = self.get_visibility()
+        self.total_visibility = sum(sum(v) for v in self.visibility)
 
-def south(forest: list[list[int]], row: int, col: int) -> list[int]:
-    return [c[col] for c in forest[row + 1:]]
+        self.scores = self.get_scores()
+        self.max_score = max(max(s) for s in self.scores)
 
+    def count_visible(self, position: Position, direction: str) -> int:
+        count = 0
+        for tree in self.look(position, direction):
+            count += 1
+            if tree >= self.trees[position.row][position.column]:
+                break
+        return count
 
-def east(forest: list[list[int]], row: int, col: int) -> list[int]:
-    return [c for c in forest[row][col + 1:]]
+    def get_scores(self) -> list[list[int]]:
+        scores = [[0] * len(self.trees[0])]
+        for row in range(1, len(self.trees) - 1):
+            scores.append([0] + [1] * (len(self.trees[row]) - 2) + [0])
+            for col in range(1, len(self.trees[row]) - 1):
+                position = Position(row, col)
+                for d in self.directions:
+                    scores[row][col] *= self.count_visible(position, d)
+        scores.append([0] * len(self.trees[-1]))
+        return scores
 
+    def get_visibility(self) -> list[list[bool]]:
+        visibility = [[True] * len(self.trees[0])]
+        for row in range(1, len(self.trees) - 1):
+            visibility.append([True] + [False] * (len(self.trees[row]) - 2) + [True])
+            for col in range(1, len(self.trees[row]) - 1):
+                position = Position(row, col)
+                visibility[row][col] = self.trees[row][col] > min(
+                    [max(self.look(position, d)) for d in self.directions]
+                )
+        visibility.append([True] * len(self.trees[-1]))
+        return visibility
 
-def west(forest: list[list[int]], row: int, col: int) -> list[int]:
-    return [c for c in reversed(forest[row][:col])]
+    directions = ["north", "south", "east", "west"]
 
+    def look(self, position: Position, direction: str) -> list[int]:
+        if direction == "north":
+            return [c[position.column] for c in reversed(self.trees[:position.row])]
+        elif direction == "south":
+            return [c[position.column] for c in self.trees[position.row + 1:]]
+        elif direction == "east":
+            return [c for c in self.trees[position.row][position.column + 1:]]
+        elif direction == "west":
+            return [c for c in reversed(self.trees[position.row][:position.column])]
+        else:
+            raise ValueError(f"Unknown direction {direction}")
 
-def get_visibility(forest: list[list[int]]) -> list[list[bool]]:
-    visibility = [[True] * len(forest[0])]
-    for row in range(1, len(forest) - 1):
-        visibility.append([True] + [False] * (len(forest[row]) - 2) + [True])
-        for col in range(1, len(forest[row]) - 1):
-            visibility[row][col] = \
-                max(north(forest, row, col)) < forest[row][col] or \
-                max(south(forest, row, col)) < forest[row][col] or \
-                max(east(forest, row, col)) < forest[row][col] or \
-                max(west(forest, row, col)) < forest[row][col]
-    visibility.append([True] * len(forest[-1]))
-    return visibility
-
-
-def count_visible(trees: list[int], height: int) -> int:
-    count = 0
-    for tree in trees:
-        count += 1
-        if tree >= height:
-            break
-    return count
-
-
-def get_scores(forest: list[list[int]]) -> list[list[int]]:
-    scores = [[0] * len(forest[0])]
-    for row in range(1, len(forest) - 1):
-        scores.append([0] * len(forest[row]))
-        for col in range(1, len(forest[row]) - 1):
-            scores[row][col] = \
-                count_visible(north(forest, row, col), forest[row][col]) * \
-                count_visible(south(forest, row, col), forest[row][col]) * \
-                count_visible(east(forest, row, col), forest[row][col]) * \
-                count_visible(west(forest, row, col), forest[row][col])
-    scores.append([0] * len(forest[-1]))
-    return scores
-
-
-def part_1() -> int:
-    visibility = get_visibility(load_forest())
-    return sum(sum(v) for v in visibility)
-
-
-def part_2() -> int:
-    forest = load_forest()
-    scores = get_scores(forest)
-    return max(max(s) for s in scores)
 
 def main():
-    print(f"Part 1: {part_1()}")
-    print(f"Part 2: {part_2()}")
+    forest = Forest(get_daily_input(DAY))
+    print(f"Part 1: {forest.total_visibility}")
+    print(f"Part 2: {forest.max_score}")
 
 
 if __name__ == "__main__":
