fde51511d1081411bf7687069b00d7f45ed90732
Jerry Gregoire <jerry@arcadecoffee.com>
2022-12-09 11:50:40 -0600

    Day 8 - Part 1 & 2

--- day09.py
+++ day09.py
@@ -34,14 +34,10 @@ class Coordinate:
     y: int = 0
 
     def adjacent(self, other) -> bool:
-        distance = self - other
-        return abs(distance.x) <= 1 and abs(distance.y) <= 1
+        return abs(self.x - other.x) <= 1 and abs(self.y - other.y) <= 1
 
     def __add__(self, other) -> "Coordinate":
         return Coordinate(x=self.x + other.x, y=self.y + other.y)
-
-    def __sub__(self, other) -> "Coordinate":
-        return Coordinate(x=self.x - other.x, y=self.y - other.y)
 
 
 @dataclass
@@ -60,49 +56,41 @@ class Rope:
         return 1 if a > b else -1 if a < b else 0
 
     def move_head(self, offset: Coordinate) -> None:
+        def _compare(a: int, b: int):
+            return (a > b) - (a < b)
+
         self.knot_paths[0].append(self.knot_paths[0][-1] + offset)
         for k in range(1, self.num_knots):
             curr_knot = self.knot_paths[k][-1]
             prev_knot = self.knot_paths[k - 1][-1]
             if not curr_knot.adjacent(prev_knot):
-                knot_offset = Coordinate(self._trinary(prev_knot.x, curr_knot.x),
-                                         self._trinary(prev_knot.y, curr_knot.y))
+                knot_offset = Coordinate(_compare(prev_knot.x, curr_knot.x),
+                                         _compare(prev_knot.y, curr_knot.y))
                 self.knot_paths[k].append(curr_knot + knot_offset)
 
 
-DIRECTIONS = {
-    "R": Coordinate(1, 0),
-    "L": Coordinate(-1, 0),
-    "U": Coordinate(0, 1),
-    "D": Coordinate(0, -1)
-}
+def pull_rope(num_knots: int = 2):
+    directions = {
+        "R": Coordinate(1, 0),
+        "L": Coordinate(-1, 0),
+        "U": Coordinate(0, 1),
+        "D": Coordinate(0, -1)
+    }
 
-
-def part_1() -> int:
-    rope = Rope()
-
-    for row in get_daily_input(DAY):
-        d, n = row.split(" ")
-        for _ in range(int(n)):
-            rope.move_head(DIRECTIONS[d])
-
-    return len(set(rope.knot_paths[-1]))
-
-
-def part_2() -> int:
-    rope = Rope(num_knots=10)
+    rope = Rope(num_knots=num_knots)
 
     for row in get_daily_input(DAY):
         d, n = row.split(" ")
         for _ in range(int(n)):
-            rope.move_head(DIRECTIONS[d])
+            rope.move_head(directions[d])
 
-    return len(set(rope.knot_paths[-1]))
+    return rope
 
 
 def main():
-    print(f"Part 1: {part_1()}")
-    print(f"Part 2: {part_2()}")
+    rope = pull_rope(num_knots=10)
+    print(f"Part 1: {len(set(rope.knot_paths[1]))}")
+    print(f"Part 2: {len(set(rope.knot_paths[-1]))}")
 
 
 if __name__ == "__main__":
