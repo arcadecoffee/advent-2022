252f9f3cc863a9353fc0417373b8d385cc9421e5
Jerry Gregoire <jerry@arcadecoffee.com>
2022-12-14 13:08:14 -0600

    Day 15 - prep

---
+++ day15.py
@@ -0,0 +1,37 @@
+"""
+Advent of Code 2022 Day 15
+"""
+import sys
+
+from advent_tools import get_daily_input
+
+DAY = 15
+
+DEBUG = sys.argv[1] == "debug" if len(sys.argv) > 1 else False
+
+DEBUG_DATA = """
+"""
+
+if DEBUG:
+    def get_daily_input(_):
+        for line in DEBUG_DATA.strip().split("\n"):
+            yield line.strip("\n")
+
+
+def part_1() -> int:
+    data = get_daily_input(DAY)
+    return len(list(data))
+
+
+def part_2() -> int:
+    data = get_daily_input(DAY)
+    return len(list(data))
+
+
+def main():
+    print(f"Part 1: {part_1()}")
+    print(f"Part 2: {part_2()}")
+
+
+if __name__ == "__main__":
+    main()
