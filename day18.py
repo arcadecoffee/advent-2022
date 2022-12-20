87e8394935907da4fd5ecd83275c609797da6f90
Jerry Gregoire <12764780+arcadecoffee@users.noreply.github.com>
2022-12-20 16:42:54 -0600

    Merge pull request #22
    * Day 18 - Part 1
    
    * Day 18 - Part 1 & Part 2 - brute force-y
    
    * Day 18 - Part 1 & Part 2 - brute force-y

--- day18.py
+++ day18.py
@@ -10,6 +10,19 @@ DAY = 18
 TEST = sys.argv[1] == "test" if len(sys.argv) > 1 else False
 
 TEST_DATA = """
+2,2,2
+1,2,2
+3,2,2
+2,1,2
+2,3,2
+2,2,1
+2,2,3
+2,2,4
+2,2,6
+1,2,5
+3,2,5
+2,1,5
+2,3,5
 """
 
 if TEST:
@@ -18,14 +31,56 @@ if TEST:
             yield line.strip("\n")
 
 
+class Point:
+    def __init__(self, x, y, z):
+        self.x, self.y, self.z = x, y, z
+
+    def __repr__(self):
+        return f"{self.x}, {self.y}, {self.z}"
+
+
 def part_1() -> int:
-    data = get_daily_input(DAY)
-    return len(list(data))
+    points = [Point(*[int(i) for i in s.split(",")]) for s in get_daily_input(DAY)]
+
+    faces = 0
+    for p_i in range(len(points)):
+        p = points[p_i]
+        for r_i in range(p_i + 1, len(points)):
+            r = points[r_i]
+            faces += 2 * (
+                    (p.x == r.x and p.y == r.y and p.z == r.z + 1) +
+                    (p.x == r.x and p.y == r.y and p.z == r.z - 1) +
+                    (p.x == r.x and p.y == r.y + 1 and p.z == r.z) +
+                    (p.x == r.x and p.y == r.y - 1 and p.z == r.z) +
+                    (p.x == r.x + 1 and p.y == r.y and p.z == r.z) +
+                    (p.x == r.x - 1 and p.y == r.y and p.z == r.z)
+            )
+    return len(points) * 6 - faces
 
 
 def part_2() -> int:
-    data = get_daily_input(DAY)
-    return len(list(data))
+    points = [tuple(int(i) for i in s.split(",")) for s in get_daily_input(DAY)]
+
+    min_range = min(min(i) for i in points) - 1
+    max_range = max(max(i) for i in points) + 2
+
+    faces = 0
+    outside_points = [(min_range, min_range, min_range)]
+    visited = []
+    while outside_points:
+        x, y, z = outside_points.pop()
+        if (x, y, z) not in visited:
+            visited.append((x, y, z))
+
+            for ox, oy, oz in [(x + 1, y, z), (x - 1, y, z),
+                               (x, y + 1, z), (x, y - 1, z),
+                               (x, y, z + 1), (x, y, z - 1)]:
+                if all(min_range <= i <= max_range for i in (ox, oy, oz)):
+                    if (ox, oy, oz) in points:
+                        faces += 1
+                    else:
+                        outside_points.append((ox, oy, oz))
+    return faces
 
 
 def main():
