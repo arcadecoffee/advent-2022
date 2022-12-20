f6a058ce5e0e8259066986be2f239fc3f0d2dc42
Jerry Gregoire <jerry@arcadecoffee.com>
2022-12-20 16:35:19 -0600

    Day 18 - Part 1 & Part 2 - brute force-y

--- day18.py
+++ day18.py
@@ -34,32 +34,63 @@ if TEST:
 class Point:
     def __init__(self, x, y, z):
         self.x, self.y, self.z = x, y, z
+        self.up = self.down = self.left = self.right = self.front = self.back = True
+
+    @property
+    def open_sides(self) -> int:
+        return sum([self.up, self.down, self.left, self.right, self.front, self.back])
+
+    def __repr__(self):
+        return f"{self.x}, {self.y}, {self.z}"
 
 
 def part_1() -> int:
     points = [Point(*[int(i) for i in s.split(",")]) for s in get_daily_input(DAY)]
 
-    total_sides = len(points) * 6
-
     for p_i in range(len(points)):
         p = points[p_i]
         for r_i in range(p_i + 1, len(points)):
             r = points[r_i]
-            total_sides -= 2 * sum([
-                p.x == r.x and p.y == r.y and p.z == r.z + 1,
-                p.x == r.x and p.y == r.y and p.z == r.z - 1,
-                p.x == r.x and p.y == r.y + 1 and p.z == r.z,
-                p.x == r.x and p.y == r.y - 1 and p.z == r.z,
-                p.x == r.x + 1 and p.y == r.y and p.z == r.z,
-                p.x == r.x - 1 and p.y == r.y and p.z == r.z,
-            ])
+            if p.x == r.x and p.y == r.y and p.z == r.z + 1:
+                p.up = r.down = False
+            if p.x == r.x and p.y == r.y and p.z == r.z - 1:
+                p.down = r.up = False
+            if p.x == r.x and p.y == r.y + 1 and p.z == r.z:
+                p.left = r.right = False
+            if p.x == r.x and p.y == r.y - 1 and p.z == r.z:
+                p.right = r.left = False
+            if p.x == r.x + 1 and p.y == r.y and p.z == r.z:
+                p.front = r.back = False
+            if p.x == r.x - 1 and p.y == r.y and p.z == r.z:
+                p.back = r.front = False
 
-    return total_sides
+    return sum(p.open_sides for p in points)
 
 
 def part_2() -> int:
-    data = get_daily_input(DAY)
-    return len(list(data))
+    points = [tuple(int(i) for i in s.split(",")) for s in get_daily_input(DAY)]
+
+    min_range = min(min(i) for i in points) - 1
+    max_range = max(max(i) for i in points) + 2
+
+    faces = 0
+    outside_points = [(min_range, min_range, min_range)]
+    visited = []
+    while outside_points:
+        x, y, z = outside_points.pop()
+        if (x, y, z) not in visited:
+            visited.append((x, y, z))
+
+            for ox, oy, oz in [(x + 1, y, z), (x - 1, y, z),
+                               (x, y + 1, z), (x, y - 1, z),
+                               (x, y, z + 1), (x, y, z - 1)]:
+                if all(min_range <= i <= max_range for i in (ox, oy, oz)):
+                    if (ox, oy, oz) in points:
+                        faces += 1
+                    else:
+                        outside_points.append((ox, oy, oz))
+
+    return faces
 
 
 def main():
