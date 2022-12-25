dc6a6e4591f31d2361f6df7e2cab0630450e07e8
Jerry Gregoire <jerry@arcadecoffee.com>
2022-12-25 13:31:14 -0600

    Day 20 - Part 1 -- works for test but not for full

--- day20.py
+++ day20.py
@@ -10,6 +10,13 @@ DAY = 20
 TEST = sys.argv[1] == "test" if len(sys.argv) > 1 else False
 
 TEST_DATA = """
+1
+2
+-3
+3
+-2
+0
+4
 """
 
 if TEST:
@@ -18,9 +25,64 @@ if TEST:
             yield line.strip("\n")
 
 
+class Element:
+    def __init__(self, value: int):
+        self.value = value
+        self.next = None
+        self.prev = None
+
+
 def part_1() -> int:
-    data = get_daily_input(DAY)
-    return len(list(data))
+    # build the circular linked-list
+    all_elements:  list[Element] = []
+    zero = None
+    for value in get_daily_input(DAY):
+        curr_element = Element(int(value))
+        if curr_element.value == 0:
+            zero = curr_element
+        if all_elements:
+            all_elements[-1].next = curr_element
+            curr_element.prev = all_elements[-1]
+        all_elements.append(curr_element)
+    all_elements[-1].next = all_elements[0]
+    all_elements[0].prev = all_elements[-1]
+
+    # start mixing
+    for e in all_elements:
+        if e.value != 0:
+            e.prev.next = e.next
+            e.next.prev = e.prev
+
+            steps = e.value % len(all_elements)
+            if e.value < 0:
+                steps -= 1
+
+            new_prev = e
+            for _ in range(steps):
+                new_prev = new_prev.next
+
+            e.prev = new_prev
+            e.next = new_prev.next
+            new_prev.next = e
+            e.next.prev = e
+
+    answer = 0
+    n = zero
+    for _ in range(3):
+        for _ in range(1000 % len(all_elements)):
+            n = n.next
+        answer += n.value
+
+    return answer
+
+
+def dump(all_elements):
+    ce = all_elements[0]
+    cv = []
+    for i in range(len(all_elements)):
+        cv.append(str(ce.value))
+        ce = ce.next
+    print(", ".join(cv))
 
 
 def part_2() -> int:
@@ -35,3 +97,7 @@ def main():
 
 if __name__ == "__main__":
     main()
+
+"""
+Part 1: 7030 is too low...
+"""
