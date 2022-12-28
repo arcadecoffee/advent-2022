83f4b84166b7bfe525e2826caf8d9c375dd0ab9c
Jerry Gregoire <jerry@arcadecoffee.com>
2022-12-28 11:35:06 -0600

    Day 22 - Part 1 - cleaner

--- day22.py
+++ day22.py
@@ -33,71 +33,71 @@ if TEST:
             yield line.strip("\n")
 
 
-def load_data():
+def load_data() -> tuple[list[str], list[str]]:
     data_stream = get_daily_input(DAY)
     map_data: list[str] = []
-    path_data: str = ""
+    path_data: list[str] = []
     for d in data_stream:
         if d:
             map_data.append(d)
         else:
-            path_data = next(data_stream)
-
+            path_data = re.findall(r"\d+|[LR]", next(data_stream))
     width = max(len(m) for m in map_data)
-    map_data = list([m.ljust(width) for m in map_data])
+    return list([m.ljust(width) for m in map_data]), path_data
 
-    return map_data, re.findall(r"\d+|[LR]", path_data)
+
+FACINGS = ">v<^"
 
 
 def part_1() -> int:
-    facings = ">v<^"
 
     monkey_map, path = load_data()
-    position = (0, monkey_map[0].find("."))
+    row, column = 0, monkey_map[0].find(".")
     facing = ">"
-
-    def next_position(r, c, f):
-        if f == ">":
-            points = monkey_map[r]
-            offset = c
-        elif f == "<":
-            points = "".join(reversed(monkey_map[r]))
-            offset = len(points) - c - 1
-        elif f == "v":
-            points = "".join([m[c] for m in monkey_map])
-            offset = r
-        else:
-            points = "".join([m[c] for m in reversed(monkey_map)])
-            offset = len(points) - r - 1
-
-        if offset < len(points) - 1 and points[offset + 1] == ".":
-            offset = offset + 1
-        elif offset >= len(points) - 1 or points[offset + 1] == " ":
-            for new_offset in range(offset - 1, -2, -1):
-                if new_offset < 0 or points[new_offset] == " ":
-                    if points[new_offset + 1] == ".":
-                        offset = new_offset + 1
-                    break
-
-        if f == ">":
-            return r, offset
-        elif f == "<":
-            return r, len(points) - offset - 1
-        elif f == "v":
-            return offset, c
-        else:
-            return len(points) - offset - 1, c
 
     for p in path:
         if p.isnumeric():
             for _ in range(int(p)):
-                position = next_position(*position, facing)
+                row, column = next_position(monkey_map, row, column, facing)
         elif p == "L":
-            facing = facings[facings.find(facing) - 1]
+            facing = FACINGS[FACINGS.find(facing) - 1]
         else:
-            facing = facings[(facings.find(facing) + 1) % 4]
+            facing = FACINGS[(FACINGS.find(facing) + 1) % 4]
 
-    return 1000 * (position[0] + 1) + 4 * (position[1] + 1) + facings.find(facing)
+    return 1000 * (row + 1) + 4 * (column + 1) + FACINGS.find(facing)
+
+
+def next_position(mm: list[str], r: int, c: int, f: str) -> tuple[int, int]:
+    if f == ">":
+        points = mm[r]
+        offset = c
+    elif f == "<":
+        points = "".join(reversed(mm[r]))
+        offset = len(points) - c - 1
+    elif f == "v":
+        points = "".join([m[c] for m in mm])
+        offset = r
+    else:
+        points = "".join([m[c] for m in reversed(mm)])
+        offset = len(points) - r - 1
+
+    if offset < len(points) - 1 and points[offset + 1] == ".":
+        offset = offset + 1
+    elif offset >= len(points) - 1 or points[offset + 1] == " ":
+        for new_offset in range(offset - 1, -2, -1):
+            if new_offset < 0 or points[new_offset] == " ":
+                if points[new_offset + 1] == ".":
+                    offset = new_offset + 1
+                break
+
+    if f == ">":
+        return r, offset
+    elif f == "<":
+        return r, len(points) - offset - 1
+    elif f == "v":
+        return offset, c
+    else:
+        return len(points) - offset - 1, c
 
 
 def part_2() -> int:
