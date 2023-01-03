0e216a4bb9e24648f4716d49e0858b7a9622a3f6
Jerry Gregoire <jerry@arcadecoffee.com>
2023-01-03 09:14:23 -0600

    Day 23 - prep

---
+++ day23.py
@@ -0,0 +1,37 @@
+"""
+Advent of Code 2022 Day 23
+"""
+import sys
+
+from advent_tools import get_daily_input
+
+DAY = 23
+
+TEST = sys.argv[1] == "test" if len(sys.argv) > 1 else False
+
+TEST_DATA = """
+"""
+
+if TEST:
+    def get_daily_input(_):
+        for line in TEST_DATA.strip().split("\n"):
+            yield line.strip("\n")
+
+
+def part_1() -> int:
+    data = get_daily_input(DAY)
+    return len(list(data))
+
+
+def part_2() -> int:
+    data = get_daily_input(DAY)
+    return len(list(data))
+
+
+def main():
+    print(f"Part 1: {part_1()}")
+    print(f"Part 2: {part_2()}")
+
+
+if __name__ == "__main__":
+    main()
