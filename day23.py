5bd59187010fdbb5f1c675f16dc48cd4912de97d
Jerry Gregoire <jerry@arcadecoffee.com>
2023-01-03 12:55:31 -0600

    Day 23 - Part 1 & Part 2

--- day23.py
+++ day23.py
@@ -25,103 +25,72 @@ if TEST:
             yield line.strip("\n")
 
 
-def load_data() -> list[list[bool]]:
-    data: list[list[bool]] = [[c == "#" for c in l] for l in get_daily_input(DAY)]
+def load_data() -> dict[tuple[int, int], bool]:
+    data = {}
+    x = 0
+    for l in get_daily_input(DAY):
+        y = 0
+        for c in l:
+            if c == "#":
+                data[x, y] = True
+            y += 1
+        x += 1
     return data
 
 
 def count_empty(map_data: list[list[bool]]) -> int:
-    return len(map_data) * len(map_data[0]) - sum([sum(m) for m in map_data])
-
-
-def dump_map(map_data: list[list[bool]]):
-    print("\n".join(["".join(["#" if c else "." for c in m]) for m in map_data]))
-
-
-def pad_map(map_data: list[list[bool]]) -> list[list[bool]]:
-    new_map: list[list[bool]] = [[False] * (len(map_data[0]) + 2)]
-    for m in map_data:
-        new_map.append([False] + m + [False])
-    new_map.append([False] * (len(map_data[0]) + 2))
-    return new_map
+    h = max([k[0] for k in map_data]) - min([k[0] for k in map_data]) + 1
+    w = max([k[1] for k in map_data]) - min([k[1] for k in map_data]) + 1
+    return h * w - len(map_data)
 
 
 def make_moves(map_data: list[list[bool]], rule_order: str) -> list[list[bool]]:
     proposed_moves: dict[tuple[int, int], list[tuple[int, int]]] = {}
 
-    for i in range(1, len(map_data) - 1):
-        for j in range(1, len(map_data[i]) - 1):
-            if map_data[i][j]:
-                proposed_moves[(i, j)] = [(i, j)]
-                surroundings: dict[str, tuple[bool, tuple[int, int]]] = {}
-                for direction in ["N", "NE", "E", "SE", "S", "SW", "W", "NW"]:
-                    r = i + (1 if "S" in direction else -1 if "N" in direction else 0)
-                    d = j + (1 if "E" in direction else -1 if "W" in direction else 0)
-                    surroundings[direction] = map_data[r][d], (r, d)
-                if any([surroundings[k][0] for k in surroundings]):
-                    for direction in rule_order:
-                        if not any([surroundings[k][0] for k in surroundings if direction in k]):
-                            proposed_moves[surroundings[direction][1]] = \
-                                proposed_moves.get(surroundings[direction][1], []) + [(i, j)]
-                            del proposed_moves[(i, j)]
-                            break
+    for i, j in map_data:
+        proposed_moves[i, j] = [(i, j)]
+        surroundings: dict[str, tuple[bool, tuple[int, int]]] = {}
+        for direction in ["N", "NE", "E", "SE", "S", "SW", "W", "NW"]:
+            r = i + (1 if "S" in direction else -1 if "N" in direction else 0)
+            d = j + (1 if "E" in direction else -1 if "W" in direction else 0)
+            surroundings[direction] = map_data.get((r, d), False), (r, d)
+        if any([surroundings[k][0] for k in surroundings]):
+            for direction in rule_order:
+                if not any([surroundings[k][0] for k in surroundings if direction in k]):
+                    proposed_moves[surroundings[direction][1]] = \
+                        proposed_moves.get(surroundings[direction][1], []) + [(i, j)]
+                    del proposed_moves[(i, j)]
+                    break
 
-    new_map = [[False] * len(m) for m in map_data]
-    for k, v in proposed_moves.items():
+    new_map = {}
+    for k, v in [(a, b) for (a, b) in proposed_moves.items()]:
         if len(v) > 1:
             for i in v:
-                new_map[i[0]][i[1]] = True
-        else:
-            new_map[k[0]][k[1]] = True
+                proposed_moves[i] = [i]
+            del proposed_moves[k]
 
-    return new_map
-
-
-def prune_map(map_data: list[list[bool]]) -> list[list[bool]]:
-    new_map = map_data.copy()
-    while not any(new_map[0]):
-        new_map.pop(0)
-    while not any(new_map[-1]):
-        new_map.pop()
-    while not any(m[0] for m in new_map):
-        for i in range(len(new_map)):
-            new_map[i] = new_map[i][1:]
-    while not any(m[-1] for m in new_map):
-        for i in range(len(new_map)):
-            new_map[i] = new_map[i][:-1]
-    return new_map
+    return proposed_moves
 
 
 def part_1() -> int:
     map_data = load_data()
     rule_order = "NSWE"
-    # print("== Initial State ==")
-    # dump_map(map_data)
     for i in range(10):
-        map_data = pad_map(map_data)
         map_data = make_moves(map_data, rule_order)
-        map_data = prune_map(map_data)
-
-        # print(f"\n== End of Round {i + 1} ==")
-        # dump_map(map_data)
-        # print()
-
         rule_order = rule_order[1:] + rule_order[0]
     return count_empty(map_data)
 
 
 def part_2() -> int:
     map_data = load_data()
-    prev_map_data = None
+    prev_map_data = {}
     rule_order = "NSWE"
-    i = 0
+    i = -1
     while prev_map_data != map_data:
-        i += 1
         prev_map_data = map_data
-        map_data = pad_map(map_data)
         map_data = make_moves(map_data, rule_order)
-        map_data = prune_map(map_data)
         rule_order = rule_order[1:] + rule_order[0]
+        i += 1
     return i
 
 
